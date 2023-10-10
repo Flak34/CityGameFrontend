0863739d70c1771dddf2ce2059d808963adbc407
Alexandr <aleksandr.tret.4@mail.ru>
2023-10-10 20:31:20 +0300

    Websocket Functionality Implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import * as SockJS from 'sockjs-client'
+import * as Stomp from 'stompjs'
+import axios from 'axios';
+
+var stompClient = null;
 
 function App() {
+
+  const [playerId, setPlayerId] = useState(sessionStorage.getItem("playerId"));
+  const [gameState, setGameState] = useState(JSON.parse(sessionStorage.getItem("game")));
+  const [word, setWord] = useState('');
+
+  useEffect(() => {
+    if(playerId === null) {
+      axios.get('http://localhost:8080/get/id').then((response) => {
+        sessionStorage.setItem('playerId', response.data.id);
+        setPlayerId(response.data.id);
+      })
+    }
+    else if(gameState !== null) {
+      connect();
+    }
+  }, []);
+
+  const connect = () => {
+    const socket = new SockJS("http://localhost:8080/ws");
+    stompClient = Stomp.over(socket);
+    stompClient.connect({login: JSON.parse(playerId)}, onConnected);
+  };
+
+  const onConnected = () => {
+    stompClient.subscribe(
+      "/user/" + playerId + "/queue/game-moves",
+      onMessageReceived
+    );
+  };
+
+  const onMessageReceived = (msg) => {
+    const message = JSON.parse(msg.body);
+    sessionStorage.setItem("game", JSON.stringify(message));
+    setGameState(message);
+  };
+
+
+  const sendWord = () => {
+    const message = {
+      gameId: gameState.gameId,
+      suggestedWord: word
+    };
+    stompClient.send("/game/move", {}, JSON.stringify(message));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {
+        gameState === null && 
+        <button onClick={connect}>Подключиться к игре</button>
+      }
+
+      {
+        gameState !== null && gameState.gameId === "-" &&
+        <div className='loaderContainer'>
+          <div>Ожидание соперника</div>
+          <div class="loader"></div>
+        </div>
+      }
+
+      {
+        gameState !== null && gameState.gameId !== "-" &&
+
+        <div>
+
+          {
+            gameState.info !== '' &&
+            <div style={{color: 'red'}}>{gameState.info}</div>
+          }
+
+          {
+            gameState.movingPlayerId == playerId && 
+            <div>
+              {
+                gameState.lastWord != '' &&
+                <div>Предыдущий введенный город: {gameState.lastWord}</div>
+              }
+              <input type='text' name='word' onChange={(event) => setWord(event.target.value)}></input>
+              <button onClick={sendWord}>ОК</button>
+            </div>
+          }
+
+          {
+            gameState.movingPlayerId != playerId && 
+            <div style={{fontSize: '30px'}}>Ход соперника</div>
+          }
+
+        </div>
+      }
+
     </div>
   );
 }
